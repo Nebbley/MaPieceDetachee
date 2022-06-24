e391b14f4e5a93bc36029b7f61e439a2670f0b7a
Nebbley <adrien.leclair@itech.fr>
2022-06-24 16:56:55 +0200

    AngularV2

--- AngularMaPieceDetachee/src/app/Components/acceuil/acceuil.component.ts
+++ AngularMaPieceDetachee/src/app/Components/acceuil/acceuil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-acceuil',
@@ -7,9 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcceuilComponent implements OnInit {
 
-  constructor() { }
+  Marques: string[];
+  Modeles: string[];
+  listpiece : string[];
+  choosed : boolean;
+  modelechoosed:boolean;
+  @ViewChild('marquechoisie') marquechoisie!: ElementRef;
+
+  constructor() {
+    this.modelechoosed =false;
+    this.Marques =[
+    "Mazda",
+    "Mercedes",
+    "Peugeot"
+  ]
+  this.Modeles=[]
+  this.choosed=false;
+  this.listpiece=[
+    "Roues",
+    "Filtres",
+    "Plaquettes"
+  ]
+  }
 
   ngOnInit(): void {
+  }
+
+  modelchoosed(){
+    if (this.Marques.includes(this.marquechoisie.nativeElement.value))  {
+    this.choosed=true;
+      if (this.marquechoisie.nativeElement.value ==="Mazda"){
+        this.Modeles = [
+          "MX5",
+          "CX5",
+          "Mazda 2"
+        ]}
+       else if (this.marquechoisie.nativeElement.value ==="Peugeot"){
+        this.Modeles = [
+          "208",
+          "206",
+          "306 "
+          ]
+      }
+    }
+    else {
+      this.choosed=false;
+    }
+
+
+  }
+  marquechoosed(){
+
+    this.modelechoosed =true;
   }
 
 }
